49ab811f88d7b1738f87bca0c545fc7089dc3796
Are133 <isad9997@gmail.com>
2021-06-10 19:52:29 -0500

    se completo el portafolio, se completo el boton para volver al inicio, y se agrego una transicion para que los elementos se revelen con una pequeña transicion

--- script.js
+++ script.js
@@ -24,3 +24,36 @@ navigationItems.forEach((navigationItem)=>{
 
 ///responsive navigation sidebar menu
 
+///js for scroll to top button
+
+const scrollBtn = document.querySelector(".scrollToTop-btn");
+
+window.addEventListener("scroll",function(){
+    scrollBtn.classList.toggle("active",this.window.scrollY>500)
+})
+
+//js for come back to top of website so click in the scroll-btn
+
+scrollBtn.addEventListener("click",()=> {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop=0;
+})
+
+//js for reveal website elements on scroll
+
+window.addEventListener("scroll",reveal);
+
+function reveal(){
+    let reveals = document.querySelectorAll(".reveal")
+
+    for(let i = 0;i<reveals.length;i++){
+        let windowsHeight = window.innerHeight;
+        let revealTop = reveals[i].getBoundingClientRect().top
+
+        let revealPoint = 50
+
+        if(revealTop<windowsHeight-revealPoint){
+            reveals[i].classList.add("active")
+        }
+    }
+}
